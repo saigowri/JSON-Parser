fafa343ea5169aca8e516c6ce02802bc4e0c8e63
Sai Gowri Kumar <saigowri.kumar@gmail.com>
2017-07-28 10:20:37 +0530

    Array class

---
+++ src/JsonParser/ArrayClass.java
@@ -0,0 +1,33 @@
+package JsonParser;
+
+import java.util.ArrayList;
+
+//Array -> Value(, Value)* | Value, Array
+
+public class ArrayClass {
+	int matchArray(ArrayList<Character> jsonString, int index) throws ParserException {
+		if (jsonString.get(index) != ']') {
+			Value v = new Value();
+			index = v.matchValue(jsonString, index);
+			while (jsonString.get(index) != ']') {
+				if (jsonString.get(index) != ',') {
+					throw new ParserException("Missing , or ] in array at location "+index);
+				}
+				index++;
+				if (index >= jsonString.size()) {
+					throw new ParserException("Missing ] for array at location "+index);
+				}
+				{
+					Value v1 = new Value();
+					index = v1.matchValue(jsonString, index);
+				}
+			}
+
+		}
+		if (jsonString.get(index) != ']') {
+			throw new ParserException("Missing ] for array at location "+index);
+		}
+		index++;
+		return index;
+	}
+}
